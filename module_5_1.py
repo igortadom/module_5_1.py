a4f31c9903931608b2df5e828bec4e7fb1c5751f
igortadom <tadom@yandex.ru>
2024-08-25 19:54:47 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,20 @@
+class House:
+    def __init__(self, name, number_of_floors):
+       self.name = name
+       self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        floor = 0
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print(f'Такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+
+
+
+h1 = House('ЖК Малахит', 9)
+h2 = House('Коттедж', 2)
+
+h1.go_to(4)
+h2.go_to(3)
